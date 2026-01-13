01dec85b5442f8a908f64a43c23b559341568751
ClayDunford <dunfordclay@gmail.com>
2026-01-13 15:56:22 -0700

    began Bishop Chess Moves

--- shared/src/main/java/chess/ChessBishopMoves.java
+++ shared/src/main/java/chess/ChessBishopMoves.java
@@ -1,5 +1,23 @@
 package chess;
 
-public class ChessBishopMoves {
+import java.util.Collection;
 
+public class ChessBishopMoves {
+    private final ChessBoard board;
+    private final ChessPosition position;
+
+    public ChessBishopMoves(ChessBoard board, ChessPosition position) {
+        this.board = board;
+        this.position = position;
+    }
+
+    public Collection<ChessMove> generateBishopMoves() {
+        ChessPiece piece = board.getPiece(position);
+        ChessGame.TeamColor color = piece.getTeamColor();
+
+        // Moving up to the right
+
+
+        return null;
+    }
 }
