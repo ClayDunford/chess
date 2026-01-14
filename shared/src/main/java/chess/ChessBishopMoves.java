9c9fe942018de00bdc7b6c2bb1cd9d3e160be1b3
ClayDunford <dunfordclay@gmail.com>
2026-01-14 14:22:26 -0700

    built Bishop moving up right

--- shared/src/main/java/chess/ChessBishopMoves.java
+++ shared/src/main/java/chess/ChessBishopMoves.java
@@ -1,6 +1,7 @@
 package chess;
 
-import java.util.Collection;
+import java.util.*;
+
 
 public class ChessBishopMoves {
     private final ChessBoard board;
@@ -14,10 +15,59 @@ public class ChessBishopMoves {
     public Collection<ChessMove> generateBishopMoves() {
         ChessPiece piece = board.getPiece(position);
         ChessGame.TeamColor color = piece.getTeamColor();
+        List<ChessMove> validMoves = new ArrayList<ChessMove>();
+        validMoves.addAll(upright());
+        validMoves.addAll(upleft());
+        return validMoves;
+    }
 
+    private ArrayList<ChessMove> upright() {
         // Moving up to the right
+        int startRow = position.getRow();
+        int startCol = position.getColumn();
+        ChessGame.TeamColor color = board.getPiece(position).getTeamColor();
+        ArrayList<ChessMove> uprightMoves = new ArrayList<ChessMove>();
+        int col = startCol + 1;
+        int row = startRow + 1;
+        while (col < 9 && row < 9) {
+            ChessPosition newPos = new ChessPosition(row, col);
+            if (board.getPiece(newPos) == null) {
+                uprightMoves.add(new ChessMove(position, newPos, null));
+                ++col;
+                ++row;
+            } else {
+                if (board.getPiece(newPos).getTeamColor() != color) {
+                    uprightMoves.add(new ChessMove(position, newPos, null));
+                    break;
+                }
+                break;
+            }
+        }
+        return uprightMoves;
+    }
 
-
-        return null;
+    private ArrayList<ChessMove> upleft() {
+        // Moving up to the right
+        int startRow = position.getRow();
+        int startCol = position.getColumn();
+        ChessGame.TeamColor color = board.getPiece(position).getTeamColor();
+        ArrayList<ChessMove> upleftMoves = new ArrayList<ChessMove>();
+        int col = startCol - 1;
+        int row = startRow + 1;
+        while (col > 0 && row < 9) {
+            ChessPosition newPos = new ChessPosition(row, col);
+            if (board.getPiece(newPos) == null) {
+                upleftMoves.add(new ChessMove(position, newPos, null));
+                --col;
+                ++row;
+            } else {
+                if (board.getPiece(newPos).getTeamColor() != color) {
+                    upleftMoves.add(new ChessMove(position, newPos, null));
+                    break;
+                }
+                break;
+            }
+        }
+        return upleftMoves;
     }
 }
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -56,9 +57,39 @@ public class ChessPiece {
         ChessPiece piece = board.getPiece(myPosition);
 
         if (piece.getPieceType() == PieceType.BISHOP) {
-            return List.of(new ChessMove(myPosition, new ChessPosition(1,8), null));
+            return new ChessBishopMoves(board,myPosition).generateBishopMoves();
         }
 
         return List.of();
+    }
+
+
+
+//    public boolean equals(Object o ){
+//        if (o == null) {
+//            return false;
+//        } if (o == this) {
+//            return true;
+//        } if (this.getClass() != o.getClass()) {
+//            return false;
+//        }
+//
+//        ChessPiece p = (ChessPiece)o;
+//        return (pieceColor.equals(p.getTeamColor()) && type.equals(p.getPieceType()));
+//    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
     }
 }
