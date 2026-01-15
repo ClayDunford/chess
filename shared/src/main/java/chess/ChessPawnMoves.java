ad05ab612e1444fb7d5fab2b7367dd6757886abf
ClayDunford <dunfordclay@gmail.com>
2026-01-14 21:28:48 -0700

    Began pawn implementation

--- shared/src/main/java/chess/ChessPawnMoves.java
+++ shared/src/main/java/chess/ChessPawnMoves.java
@@ -1,4 +1,48 @@
 package chess;
 
+import java.util.*;
 public class ChessPawnMoves {
+
+    private final ChessBoard board;
+    private final ChessPosition position;
+
+    public ChessPawnMoves(ChessBoard board, ChessPosition position) {
+        this.board = board;
+        this.position = position;
+    }
+
+    public Collection<ChessMove> generatePawnMoves() {
+        List<ChessMove> validMoves = new ArrayList<>();
+        return validMoves;
+    }
+
+    private ArrayList<ChessMove> moveDir(int rowDir, int colDir) {
+        // colDir determines the direction the columns move, +1 = up, -1 = down
+        // rowDir determines the direction the rows move, +1 = right, -1 = left
+        int startRow = position.getRow();
+        int startCol = position.getColumn();
+        ChessGame.TeamColor color = board.getPiece(position).getTeamColor();
+        ArrayList<ChessMove> moves = new ArrayList<>();
+        int col = startCol + colDir;
+        int row = startRow + rowDir;
+        ChessPosition newPos = new ChessPosition(row, col);
+
+        if (col >= 9 || col <= 0) { // || row >= 9 || row <= 0) {
+            return moves;
+        }
+        if (startCol + colDir != startCol) {
+            if (board.getPiece(newPos).getTeamColor() != color) {
+                moves.add(new ChessMove(position, newPos, null));
+            } return moves;
+        }
+
+
+        if (board.getPiece(newPos) == null) {
+            moves.add(new ChessMove(position, newPos, null));
+        } else {
+            if (board.getPiece(newPos).getTeamColor() != color) {
+                moves.add(new ChessMove(position, newPos, null));
+            }
+        } return moves;
+    }
 }
