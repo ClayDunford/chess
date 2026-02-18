8033dd455dc59e3f559a60c0f25fc8c01f08b6aa
Clay Dunford <dunfordclay@gmail.com>
2026-02-18 14:22:02 -0700

    removed debug code

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -139,15 +139,12 @@ public class ChessGame {
         /*Finds every piece and checks if they have any valid moves
         True if they do have valid moves False if they do not
          */
-        Collection<ChessPiece> debugPiecesMoves = new ArrayList<>();
         for(int col = 1; col < 9; col++) {
             for (int row = 1; row < 9; row++) {
                 ChessPosition curPosition = new ChessPosition(row, col);
                 ChessPiece curPiece = board.getPiece(curPosition);
                 if (curPiece != null && curPiece.getTeamColor() == color) {
-                        Collection<ChessMove> moves = validMoves(curPosition);
                     if (!validMoves(curPosition).isEmpty()) {
-                        debugPiecesMoves.add(curPiece);
                         return true;
                     }
                 }
@@ -164,9 +161,6 @@ public class ChessGame {
      */
     public boolean isInCheckmate(TeamColor teamColor) {
         // Write move checking for every piece on the board that works for checkmate and stalemate
-        boolean check = isInCheck(teamColor);
-        boolean pieces = pieceChecker(teamColor);
-        int egg = 1;
         return isInCheck(teamColor) && !pieceChecker(teamColor);
     }
 
