1a9a891d36b8edc72a1e0343d4a01f4b4e6b4cd5
Clay Dunford <dunfordclay@gmail.com>
2026-02-21 14:41:28 -0700

    Moved a bunch of stuff around

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,30 @@
+package server;
+
+import io.javalin.*;
+import io.javalin.http.Context;
+
+public class Server {
+
+    private final Javalin javalin;
+
+    public Server() {
+        javalin = Javalin.create(config -> config.staticFiles.add("web"))
+                .post("/user", this::registerUser);
+
+        // Register your endpoints and exception handlers here.
+
+    }
+
+    public int run(int desiredPort) {
+        javalin.start(desiredPort);
+        return javalin.port();
+    }
+
+    public void stop() {
+        javalin.stop();
+    }
+
+    private void registerUser(Context ctx) {
+
+    }
+}
